65983fb34ec0592bfb58ac2d802969ac00a16777
BoonruksaBL036 <664259036@webmail.npru.ac.th>
2025-08-19 10:29:58 +0700

    ปรับการ์ดเิพ่มลิงก์ในไฟล์ db.json

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,3 +1,14 @@
 import { createBrowserRouter } from "react-router";
-const router = createBrowserRouter([]);
+import Add from "../pages/Add";
+import Home from "../pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/add",
+    element: <Add />,
+  },
+]);
 export default router;
